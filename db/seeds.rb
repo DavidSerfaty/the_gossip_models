9eadce40fc24a9f9a9874a35607494ef41eeef3f
DavidSerfaty <david@owanted.io>
2020-10-22 20:36:17 +0200

    Add City Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,17 @@ require 'faker'
 
 User.destroy_all
 
+cities = Array.new
+users = Array.new
+
 10.times do
-  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 10), email: Faker::Internet.email, age: Faker::Number.within(range: 18..70))
+  city = City.create(name: Faker::Address.city, zip_code: Faker::Address.postcode)
+  cities << city
+end
+puts "10 Cities créées"
+
+10.times do
+  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 10), email: Faker::Internet.email, age: Faker::Number.within(range: 18..70), city: cities.sample)
+  users << user
 end
 puts "10 Users créés"
